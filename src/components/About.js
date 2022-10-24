6a1233012cb3923fe3df5408df3c6dac490101af
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-24 14:03:35 -0600

    finish about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. I'm Brian, nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            Poop in the plant pot jump on fridge i is not fat, i is fluffy
+            really likes hummus for ears back wide eyed but carrying out
+            surveillance on the neighbour's dog growl at dogs in my sleep. Stare
+            at ceiling light you have cat to be kitten me right meow.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
