f0881add58aa869eb3ee41c7326f50a9ab67fb72
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-25 16:12:37 -0600

    firebase hosting setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,10 @@ function Home() {
           I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a full stack developer specializing in building ...
+          Enthusiastic full stack web developer recently graduated from
+          Lighthouse Labs with a diploma in full stack web development. Looking
+          for an opportunity to join a team and deliver production grade
+          applications.
         </p>
         <div>
           <Link to="projects" smooth={true} duration={500}>
