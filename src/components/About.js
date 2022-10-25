69ad84385627bc470e7ad6233a43f04fc7e662a8
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-25 16:17:15 -0600

    test to see if commit updates to firebase hosting

--- src/components/About.js
+++ src/components/About.js
@@ -16,16 +16,7 @@ function About() {
           <div className="sm:text-right text-4xl font-bold">
             <p>Hi. I'm Brian, nice to meet you. Please take a look around.</p>
           </div>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nulla
-            odio, volutpat eu semper in, tincidunt quis velit. Nunc quam mauris,
-            eleifend a lacus a, pharetra ultrices lacus. Ut ultrices lacus ut
-            enim ultrices tincidunt. Duis maximus pulvinar ipsum, ac rhoncus
-            orci placerat et. Curabitur elit tellus, fringilla vitae ante vel,
-            bibendum convallis nunc. Donec commodo lacus sed leo volutpat
-            laoreet. Vestibulum ac odio quis lacus pellentesque malesuada. Donec
-            ac enim aliquam, tincidunt nunc et, accumsan elit.
-          </div>
+          <div>FILLER</div>
         </div>
       </div>
     </div>
