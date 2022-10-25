f2682224fe134abc26ed10b3f81bff2ac6778c96
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-24 22:44:10 -0600

    create file to render projects

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import homeFinderImg from "../assets/homeFinderImg.png";
+import SchedulerImg from "../assets/scheduler.png";
+import PasswordKeeper from "../assets/PasswordKeeper.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "Home Finder",
+    image: homeFinderImg,
+    github: "",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "Scheduler",
+    image: SchedulerImg,
+    github: "",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "PasswordKeeper",
+    image: PasswordKeeper,
+    github: "",
+    live: "",
+  },
+];
